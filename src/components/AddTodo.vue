78f78d93e53de484fa99f247b699d13b8b44434d
Escandon <aleskndon@hotmail.com>
2021-02-21 22:12:42 -0600

    new: todo app

---
+++ src/components/AddTodo.vue
@@ -0,0 +1,35 @@
+<template>
+	<div>
+		<form>
+			<input type="text" v-model="title" placeholder="Add to do..." />
+			<button :click="addNewTodo"> add </button>
+		</form>
+		<br />
+	</div>
+</template>
+
+<script>
+import { ref } from "vue";
+import { useStore } from "vuex";
+
+export default {
+	name: "AddTodo",
+	setup() {
+		const store = useStore();
+		const title = ref("");
+		const addNewTodo = () => {
+			// e.preventDefault();
+			store.dispatch("onAddTodo", {
+				title: title.value,
+			});
+			title.value = "";
+		};
+		return {
+			title,
+			addNewTodo,
+		};
+	},
+};
+</script>
+
+<style></style>
---
+++ src/components/Todo.vue
@@ -0,0 +1,33 @@
+<template>
+	<div>
+		<div class="todos">
+			<div v-for="todo in todos" :key="todo.id" class="todo">
+				<p> {{ todo.title }} </p>
+			</div>
+		</div>
+	</div>
+</template>
+
+<script>
+import { useStore } from "vuex"
+import { computed, onMounted }  from "vue"
+
+export default {
+	name: "Todos",
+	setup(){
+		const store = useStore()
+		const todos = computed(() => store.state.todos)
+		onMounted(() => {
+			store.dispatch("onFetchTodos")
+		});
+		return {
+			todos
+		}
+	}
+}
+
+</script>
+
+<style>
+
+</style>
